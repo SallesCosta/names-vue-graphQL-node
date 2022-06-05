557a2c2878cb4ecf08846b69f44c5d1bb3667bfb
SallesCosta <newcapital.in@gmail.com>
2022-06-05 18:51:03 +0200

    feat: integracao com site externo

--- src/App.vue
+++ src/App.vue
@@ -69,17 +69,13 @@
                         {{ sufix }}
                       </div>
                       <div class="col-md text-right">
-                        <button
-                          class="btn btn-info"
-                          v-on:click="remove(sufix)"
-                        >
+                        <button class="btn btn-info" v-on:click="remove(sufix)">
                           <span class="fa fa-trash"></span>
                         </button>
                       </div>
                     </div>
                   </li>
                 </ul>
-
                 <br />
                 <div class="input-group">
                   <input
@@ -109,9 +105,24 @@
             <ul
               class="list-group"
               v-for="domain in domains"
-              v-bind:key="domain"
+              v-bind:key="domain.name"
             >
-              <li class="list-group-item">{{ domain }}</li>
+              <li class="list-group-item">
+                <div class="row">
+                  <div class="col-md">
+                    {{ domain.name }} 
+                  </div>
+                  <div class="col-md text-right">
+                    <a
+                      class="btn btn-info"
+                      v-bind:href='domain.checkout'
+                      target="_blank"
+                    >
+                      <span class="fa fa-shopping-cart"></span>
+                    </a>
+                  </div>
+                </div>
+              </li>
             </ul>
           </div>
         </div>
@@ -130,36 +141,38 @@ export default {
     return {
       pre: '',
       suf: '',
-      prefixes: ['Air', 'Jet', 'Flight'],
-      sufixes: ['Hub', 'Station', 'Mart'],
-      domains: [],
+      prefixes: ['Air', 'Flight', 'Jet'],
+      sufixes: ['Abc', 'Bcd', 'Cde'],
     }
   },
   methods: {
     addPrefix(p) {
       this.prefixes.push(p)
       this.pre = ''
-      this.generate()
     },
     addSufix(s) {
       this.sufixes.push(s)
       this.suf = ''
-      this.generate()
-    },
-    generate() {
-      this.domains = []
-      for (const prefix of this.prefixes) {
-        for (const sufix of this.sufixes) {
-          this.domains.push(prefix + sufix)
-        }
-      }
     },
     remove(el) {
       const verificacao = this.prefixes.includes(el)
       verificacao
         ? this.prefixes.splice(this.prefixes.indexOf(el), 1)
         : this.sufixes.splice(this.sufixes.indexOf(el), 1)
-      this.generate()
+    },
+  },
+  computed: {
+    domains() {
+      console.log('aqui...')
+      const domains = []
+      for (const prefix of this.prefixes) {
+        for (const sufix of this.sufixes) {
+          const name = prefix + sufix
+          const checkout = `https://cart.hostgator.com.br/?pid=d&sld=${name.toLowerCase()}&tld=.com&domainCycle=3&_ga=2.1724209.164945295.1654446855-1077287454.1654446855`
+          domains.push({ name, checkout })
+        }
+      }
+      return domains
     },
   },
 }
